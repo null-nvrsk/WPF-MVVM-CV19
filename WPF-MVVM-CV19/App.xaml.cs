3c1a949b387876b0e1432141dbce5ca66feee61b
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-02-22 20:24:57 +0300

    Remove unnecessory files

--- WPF-MVVM-CV19/App.xaml.cs
+++ WPF-MVVM-CV19/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace WPF_MVVM_CV19
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
