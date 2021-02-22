7d37a141cd4fc5541a6a8de022e00a42930afc6b
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-02-22 23:20:44 +0300

    Testing work main window ViewModel

---
+++ WPF-MVVM-CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPF_MVVM_CV19.ViewModels.Base;
+
+namespace WPF_MVVM_CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Window title
+
+        private string _Title = "Анализ статистики CV19";
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+
+    }
+}
