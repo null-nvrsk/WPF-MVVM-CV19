0a17c9a23d637815a98ea1d81f9cec2c37f0db3e
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-03-13 11:45:53 +0300

    Move Exit command to separate class

---
+++ WPF-MVVM-CV19/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using WPF_MVVM_CV19.Infrastructure.Commands.Base;
+
+namespace WPF_MVVM_CV19.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
