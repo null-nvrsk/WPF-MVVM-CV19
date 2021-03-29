a354e81aa19b4e0a42280187e2e1e1e85f274d47
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-03-29 06:11:43 +0300

    Тестовая модель студентов

---
+++ WPF-MVVM-CV19/Models/Decanat/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_MVVM_CV19.Models.Decanat
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+        public double Rating { get; set; }
+    }
+
+    internal class Group
+    {
+        public string Name { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
